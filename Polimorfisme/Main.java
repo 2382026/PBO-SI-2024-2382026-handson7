10394c7807f2452809cce39e36ab4ed05d5675b5
2382026 <2382026@unai.edu>
2024-10-29 19:04:26 +0700

    Handson 7

--- Polimorfisme/Main.java
+++ Polimorfisme/Main.java
@@ -8,7 +8,13 @@ public class Main {
         System.out.println("Keliling Persegi: " + bangunDatar.keliling());
 
         // Lingkaran
+        bangunDatar = new Lingkaran(9);
+        System.out.println("Luas Lingkaran: " + bangunDatar.luas());
+        System.out.println("Keliling Lingkaran: " + bangunDatar.keliling());
 
         // Segitiga sama sisi
+        bangunDatar = new SegitigaSamaSisi(6);
+        System.out.println("Luas Segitiga Sama Sisi: " + bangunDatar.luas());
+        System.out.println("Keliling Segitiga Sama Sisi: " + bangunDatar.keliling());
     }
 }
